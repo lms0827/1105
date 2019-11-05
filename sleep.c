e8ebd50c08b5b8b4e869bbc8510e9dfea4b789e6
lms0827 <54874505+lms0827@users.noreply.github.com>
2019-11-04 18:20:53 -0800

    Add files via upload

---
+++ sleep.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+ 
+        printf("HELLO\n");
+        sleep(atoi(argv[1]));
+        printf("BYE\n");
+}
+                
+
+
+
+
+
+
+
